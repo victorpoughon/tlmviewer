835fff123ff4ee88b66df952e81ff414a51ada2d
Victor Poughon <victor.poughon@gmail.com.com>
2025-01-13 10:56:41 +0100

    split tests

--- test/test.ts
+++ test/test.ts
@@ -1,20 +1,37 @@
 import { tlmviewer } from "../src/main.ts";
 
+const all_tests = [
+    "test1.json",
+    "test2.json",
+    "test3.json",
+    "test4.json",
+    "test5.json",
+    "test6.json",
+];
 
-function runTests(json) {
+function runTest(json, test_file) {
     const main = document.getElementsByTagName("main")[0];
 
-    for (const test of json) {
-        const div = document.createElement("div");
-        main.appendChild(div);
+    const container = document.createElement("div");
+    main.appendChild(container);
 
-        tlmviewer(div, JSON.stringify(test));
-    }
+    const source = document.createElement("h2");
+    container.appendChild(source);
+    source.innerHTML = test_file;
+
+    const viewer = document.createElement("div");
+    viewer.classList.add("tlmviewer");
+    container.appendChild(viewer);
+
+    tlmviewer(viewer, JSON.stringify(json));
 }
-
-console.log("hello from test.ts");
 
 // Use window.onload to ensure the DOM is fully loaded
 window.onload = () => {
-    fetch("/tests.json").then(response => response.json()).then(runTests);
+    console.log("loading tlmviewer tests");
+    for (const test_file of all_tests) {
+        fetch("/tests/" + test_file)
+            .then((response) => response.json())
+            .then((data) => runTest(data, test_file));
+    }
 };
