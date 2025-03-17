52ce94d064f49dfdceb2185a3ac6f3569c8e436c
Victor Poughon <victor.poughon@gmail.com>
2025-03-17 10:01:26 +0100

    add version number to build artifact

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+// Vite plugins
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import version from "vite-plugin-package-version";
 
 export default defineConfig({
     build: {
         lib: {
-            entry: resolve(__dirname, 'src/main.ts'),
-            name: 'tlmviewer',
-            fileName: 'tlmviewer',
-            formats: ['es'],
+            entry: resolve(__dirname, "src/main.ts"),
+            name: "tlmviewer",
+            fileName: (format) => `tlmviewer-${process.env.npm_package_version}.js`,
+            formats: ["es"],
         },
     },
-    plugins: [
-        cssInjectedByJsPlugin(),
-    ]
-})
+    plugins: [cssInjectedByJsPlugin(), version()],
+});
