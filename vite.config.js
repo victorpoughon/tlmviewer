a83cf08b0ec3e44245b6066b46bc33977eb28296
Victor Poughon <victor.poughon@gmail.com>
2025-04-01 17:13:50 +0200

    add generate manifest custom plugin

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,29 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import path from "node:path";
 
 // Vite plugins
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import version from "vite-plugin-package-version";
+import testJsonManifestPlugin from "./plugins/generate-test-manifest.js";
 
 export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, "src/main.ts"),
             name: "tlmviewer",
-            fileName: (format) => `tlmviewer-${process.env.npm_package_version}.js`,
+            fileName: (format) =>
+                `tlmviewer-${process.env.npm_package_version}.js`,
             formats: ["es"],
         },
     },
-    plugins: [cssInjectedByJsPlugin(), version()],
+    plugins: [
+        cssInjectedByJsPlugin(),
+        version(),
+        testJsonManifestPlugin({
+            root: __dirname,
+            tests: "public/tests",
+            manifest: "testscenes.json",
+        }),
+    ],
 });
