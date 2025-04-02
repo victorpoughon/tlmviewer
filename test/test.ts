a39ac33bdc3421582a723eace5f1adb4ac542c28
Victor Poughon <victor.poughon@gmail.com>
2025-04-02 15:30:56 +0200

    add selector to load tests

--- test/test.ts
+++ test/test.ts
@@ -8,18 +8,9 @@ async function fetchManifest(): Promise<any> {
         });
 }
 
-function runTest(json, test_file, testContainer) {
-    const source = document.createElement("h3");
-    testContainer.appendChild(source);
-    source.innerHTML = test_file;
-
-    const viewerElement = document.createElement("div");
-    viewerElement.style.height = "450px";
-    viewerElement.style.width = "800px";
-    viewerElement.classList.add("tlmviewer");
-    testContainer.appendChild(viewerElement);
-
-    tlmviewer.embed(viewerElement, JSON.stringify(json));
+function loadMainViewer(test_file) {
+    const viewerElement = document.getElementById("main-viewer") as HTMLElement;
+    tlmviewer.load(viewerElement, test_file);
 }
 
 // Use window.onload to ensure the DOM is fully loaded
@@ -28,39 +19,20 @@ window.onload = async () => {
     const all_tests = (await fetchManifest()).toSorted();
     console.log(`loaded ${all_tests.length} json test files from manifest`);
 
-    const mainEmbed = document.getElementById("embed-tests");
-    const mainLoad = document.getElementById("load-tests");
-
-    if (mainEmbed == null || mainLoad == null) {
-        throw Error("can't find expected divs");
-    }
-
-    // test tlmviewer.embed
+    const ul = document.getElementById("tests-list") as HTMLElement;
     for (const test_file of all_tests) {
-        const testContainer = document.createElement("div");
-        mainEmbed.appendChild(testContainer);
-
-        fetch(test_file)
-            .then((response) => response.json())
-            .then((data) => runTest(data, test_file, testContainer));
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.href = "#";
+        a.textContent = test_file;
+        a.addEventListener('click', (event) => {
+            event.preventDefault();
+            loadMainViewer(test_file);
+          });
+        li.appendChild(a);
+        ul.appendChild(li);
     }
 
-    // test tlmviewer.load
-    for (const test_file of all_tests) {
-        const viewerElement = document.createElement("div");
-        viewerElement.style.height = "450px";
-        viewerElement.style.width = "800px";
-        viewerElement.classList.add("tlmviewer");
-        mainLoad.appendChild(viewerElement);
 
-        tlmviewer.load(viewerElement, test_file).then(() => {
-            // override title
-            viewerElement.getElementsByClassName(
-                "tlmviewer-title"
-            )[0].innerHTML = test_file;
-        });
-    }
-
-    // test tlmviewer.loadAll
     tlmviewer.loadAll();
 };
